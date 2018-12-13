c44b2e5a29986354313edfb8642a8351fc14d1c0
Jeremy Tarver <jtarver@partnersoft.com>
2018-12-13 12:03:05 -0500

    Cleanup

--- lambda/createAMI.js
+++ lambda/createAMI.js
@@ -1,25 +1,38 @@
-//Init AWS
+// Init AWS
 var aws = require('aws-sdk');  
 var ec2 = new aws.EC2();  
+// OPTION - limit by region
+// aws.config.region = 'eu-west-1';
 
-//Variables for the script
-//Changes below are not required but if you do change, then change to match delete and create lambda scripts.
+// Variables for the script
+// Changes below are not required but if you do change, then change to match delete and create lambda scripts.
 const keyForEpochMakerinAMI = "DATETODEL-";
-const keyForInstanceTagToBackup = "Backup"; //looks for string yes
-const keyForInstanceTagDurationBackup = "BackupRetentionDays"; //accepts numbers like 5 or 10 or 22 and so on.
-const keyForInstanceTagNoReboot = "BackupNoReboot"; //if true then it wont reboot. If not present or set to false then it will reboot.
+const keyForInstanceTagToBackup = "Backup"; // looks for string yes
+const keyForInstanceTagDurationBackup = "BackupRetentionDays"; // accepts numbers like 5 or 10 or 22 and so on.
+const keyForInstanceTagNoReboot = "BackupNoReboot"; // if true then it wont reboot. If not present or set to false then it will reboot.
 
 
-//Lambda handler
+// Lambda handler
 exports.handler = function(event, context) { 
     
     var instanceparams = {
-        Filters: [{
-            Name: 'tag:' + keyForInstanceTagToBackup,
-            Values: [
-                'yes'
-            ]
-        }]
+        Filters: [
+        	{
+	            Name: 'tag:' + keyForInstanceTagToBackup,
+	            Values: [
+	                'yes'
+	            ]
+	        },
+	        /*
+		    // OPTION - Add additional filters 
+		    {
+		        Name: 'tag:someOtherTagName',
+		        Values: [
+		            'someValue'
+		        ]
+		    }
+	    	*/
+        ]
     };
     
     ec2.describeInstances(instanceparams, function(err, data) {
@@ -42,13 +55,13 @@ exports.handler = function(event, context) {
                             }
                         }                        
                     }
-                    //cant find when to delete then dont proceed.
+                    // cant find when to delete then dont proceed.
                     if(backupRetentionDaysforAMI < 1){
                         console.log("Skipping instance Name: " + name + " backupRetentionDaysforAMI: " + backupRetentionDaysforAMI + " (backupRetentionDaysforAMI > 0)" + (backupRetentionDaysforAMI > 0));
                     }else{
                         console.log("Processing instance Name: " + name + " backupRetentionDaysforAMI: " + backupRetentionDaysforAMI + " (backupRetentionDaysforAMI > 0)" + (backupRetentionDaysforAMI > 0));                        
                         var genDate = new Date();  
-                        genDate.setDate(genDate.getDate() + backupRetentionDaysforAMI); //days that are required to be held
+                        genDate.setDate(genDate.getDate() + backupRetentionDaysforAMI); // days that are required to be held
                         var imageparams = {
                             InstanceId: instanceid,
                             Name: name + "_" + keyForEpochMakerinAMI + genDate.getTime(),
